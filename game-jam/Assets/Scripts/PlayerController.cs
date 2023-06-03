8873448b23dd32c87d0257e2e14c44e0e63c1dab
hollysavin <hollysavin26@gmail.com>
2023-06-03 14:22:29 +0100

    Player Controller added
    Basic player controller using Rigidbody
    - Movement using WASD
    - Rotation
    - Physics working when on conveyer belt

--- game-jam/Assets/Scripts/PlayerController.cs
+++ game-jam/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody rb = null;
+
+    Vector3 playerMoveInput = Vector3.zero;
+    [SerializeField] float movementMultiplyer;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.MovePosition(transform.position + playerMoveInput * Time.deltaTime * movementMultiplyer);
+
+        if (playerMoveInput == Vector3.zero)
+        {
+            return;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(playerMoveInput);
+        targetRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 360 * Time.deltaTime);
+        rb.MoveRotation(targetRotation);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+       playerMoveInput = new Vector3(context.ReadValue<Vector2>().x, 0.0f, context.ReadValue<Vector2>().y);
     }
 }
