ffbfcf3c2a1408502d5c7b57d52ccf46d5d7b89f
Jonny Ashton <135386226+mojokage@users.noreply.github.com>
2023-06-03 14:06:19 +0100

    Basic SweepItem script (raycast)
    Basic SweepItem script (raycast)

---
+++ game-jam/Assets/Scripts/SweepItem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SweepItem : MonoBehaviour
+{
+    private Color HighlightColor = Color.red;
+    private Color initialColor;
+
+    private Collider hitObject;
+    private Material hitObjectMaterial;
+
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Item"))
+        {
+            if (hitObject != null)
+                hitObjectMaterial.color = initialColor;
+            hitObject = hit.collider;
+            hitObjectMaterial = hitObject.GetComponent<Renderer>().material;
+            hitObjectMaterial.color = HighlightColor;
+            Debug.Log("raycast hit");
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                hitObject.GetComponent<Rigidbody>().AddForce(-200, 0, 0);
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                hitObject.GetComponent<Rigidbody>().AddForce(200, 0, 0);
+            }
+        }
+        else if (hitObject != null)
+        {
+            hitObjectMaterial.color = initialColor;
+            hitObject = null;
+            hitObjectMaterial = null;
+            Debug.Log("left raycast");
+        }
+    }
+}
